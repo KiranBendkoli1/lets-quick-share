5baaec68480037ad60270ac50a3f22d5e34e20f9
KiranBendkoli1 <bendkolikiran@kbtcoe.org>
2022-02-05 18:25:49 +0530

    initial app

---
+++ android/app/src/main/kotlin/com/example/lets_quick_share/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.coderays.lets_quick_share
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialization of firebase app
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
